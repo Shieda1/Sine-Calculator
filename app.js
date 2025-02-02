f518aa183b612ac5d44cfe9f3b2b857b3f657115
Shieda <shieda@Shiedas-MacBook-Air.local>
2025-02-02 22:08:13 +0300

    final commit

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-// https://calculator.swiftutors.com/sine-calculator.html
-
 const v1 =  document.getElementById('v1');
 const v2 = document.getElementById('v2');
-const v3 = document.getElementById('v3');
-const btn = document.getElementById('btn');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
 const result = document.getElementById('result');
 
 // radio buttons
@@ -43,16 +41,36 @@ hypotenuseAngleRadio.addEventListener('click', function() {
   result.textContent = '';
 });
 
-btn.addEventListener('click', function() {
+btn1.addEventListener('click', function() {
   
-  if(sineAngleRadio.checked)
-    result.textContent = `Sine Angle = ${computeSineAngle().toFixed(2)}`;
+  if(sineAngleRadio.checked) {
+    if(isNaN(computeSineAngle()))
+      result.textContent = "wrong input";
+    else
+      result.textContent = `Sine Angle = ${computeSineAngle().toFixed(4)}`;
+  }
 
-  else if(oppositeAngleRadio.checked)
-    result.textContent = `Opposite Angle = ${computeOppositeAngle().toFixed(2)}`;
+  else if(oppositeAngleRadio.checked) {
+    if(isNaN(computeOppositeAngle()))
+      result.textContent = "wrong input";
+    else
+      result.textContent = `Opposite Angle = ${computeOppositeAngle().toFixed(4)}`;
+  }
+    
 
-  else if(hypotenuseAngleRadio.checked)
-    result.textContent = `Hypotenuse Angle = ${computeHypotenuseAngle().toFixed(2)}`;
+  else if(hypotenuseAngleRadio.checked) {
+    if(isNaN(computeHypotenuseAngle()))
+      result.textContent = "wrong input";
+    else
+      result.textContent = `Hypotenuse Angle = ${computeHypotenuseAngle().toFixed(4)}`;
+  }
+})
+
+btn2.addEventListener('click', function() {
+  
+  v1.value = '';
+  v2.value = '';
+  result.textContent = '';
 })
 
 // calculation
@@ -68,3 +86,35 @@ function computeOppositeAngle() {
 function computeHypotenuseAngle() {
   return Number(oppositeAngle.value) / Number(sineAngle.value);
 }
+
+// design
+
+const nav = document.querySelector(".nav"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
+
+// progress
+
+var scroll = this.document.querySelector('.scrollTop');
+
+window.addEventListener('scroll', function() {
+  scroll.classList.toggle('active', window.scrollY > 150)
+
+  
+})
+
+scroll.addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+})
+
+// end progress
